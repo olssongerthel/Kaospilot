d78db95d5caab2cd6bf64dfda20e66bf85a87316
Adam Gerthel <insats@gmail.com>
2015-10-01 12:39:01 +0200

    Code cleanup

--- server.js
+++ server.js
@@ -5,24 +5,26 @@ var fs = require('fs'),
 // Create a namespace that will hold the plugins.
 var plugins = {};
 
+console.log('Let the Kaos begin!');
+
 // Load all available plugins.
 loader.load('./plugins/**/plugin.js', function (plugin, filename) {
-  // Bail if it appears to be a broken och incomplete plugin.
+  // Bail if it appears to be a broken or incomplete plugin.
   if (!plugin.machine || !plugin.cron || !plugin.pilot) {
     console.log('Plugin failed to load.');
-    return
+    return;
   }
   console.log('Loaded plugin: ' + plugin.label);
   plugins[plugin.machine] = plugin;
 }).then(function() {
+  console.log('All plugins loaded.');
   // Start the cron daemon
   initialize();
 });
 
+// Creates a new cron task for each plugin found.
 var initialize = function() {
   for (var plugin in plugins) {
-    new CronJob(plugins[plugin].cron, function() {
-      plugins[plugin].pilot();
-    }, null, true);
+    new CronJob(plugins[plugin].cron, plugins[plugin].pilot, null, true);
   }
 };
