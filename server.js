6c43e7ca6e79d64a6c2a22692eef200e1cd5c985
Adam Gerthel <insats@gmail.com>
2015-12-01 22:56:10 +0100

    Added the ability to run a single pilot using environment variables.

--- server.js
+++ server.js
@@ -3,41 +3,36 @@ var fs = require('fs'),
     kaospilot = require('./app/kaospilot'),
     CronJob = require('cron').CronJob;
 
-// Create a namespace that will hold the plugins.
-var plugins = [];
-
-kaospilot.log({
-  msg: 'Let the Kaos begin!'
-});
-
-// Load all available plugins.
-loader.load('./plugins/**/plugin.js', function (plugin, filename) {
-  // Bail if it appears to be a broken or incomplete plugin.
-  if (!plugin.machine || !plugin.cron || !plugin.pilot) {
-    kaospilot.log({
-      level: 'error',
-      msg: 'Plugin failed to load'
-    });
-    return;
-  }
+var run = function() {
   kaospilot.log({
-    msg: 'Loaded plugin: ' + plugin.label
+    msg: 'Let the Kaos begin!'
   });
-  plugins.push(plugin);
-}).then(function() {
-  if (plugins.length) {
+
+  // Load all available plugins.
+  loader.load('./plugins/**/plugin.js', function (plugin, filename) {
+    // Bail if it appears to be a broken or incomplete plugin.
+    if (!pluginTest(plugin)) {
+      return;
+    }
     kaospilot.log({
-      msg: 'All plugins loaded.'
+      msg: 'Loaded plugin: ' + plugin.label
     });
-    // Start the cron daemon
-    initialize();
-  } else {
-    kaospilot.log({
-      level: 'error',
-      msg: 'No plugins found. Aborting...'
-    });
-  }
-});
+    plugins.push(plugin);
+  }).then(function() {
+    if (plugins.length) {
+      kaospilot.log({
+        msg: 'All plugins loaded.'
+      });
+      // Start the cron daemon
+      initialize();
+    } else {
+      kaospilot.log({
+        level: 'error',
+        msg: 'No plugins found. Aborting...'
+      });
+    }
+  });
+}
 
 // Creates a new cron task for each plugin found.
 var initialize = function() {
@@ -45,3 +40,42 @@ var initialize = function() {
     new CronJob(plugins[i].cron, plugins[i].pilot, null, true);
   }
 };
+
+// Plugin tester
+var pluginTest = function(plugin) {
+  if (!plugin.machine || !plugin.cron || !plugin.pilot) {
+    kaospilot.log({
+      level: 'error',
+      msg: 'Plugin failed to load.'
+    });
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+// Single player mode (running a single plugin pilot once).
+if (process.env.run) {
+  kaospilot.log({
+    msg: 'Running Kaospilot in single player mode..'
+  });
+
+  // Load the plugin.
+  loader.load('./plugins/' + process.env.run + '/plugin.js', function (plugin, filename) {
+    if (!pluginTest(plugin)) {
+      return
+    }
+    // Run the plugin's pilot.
+    kaospilot.log({
+      msg: 'Executing ' + plugin.label
+    });
+    plugin.pilot();
+  });
+}
+// Continue Kaospilot as usual
+else {
+  // Create a namespace that will hold the plugins.
+  var plugins = [];
+  run();
+}
